f6d2499e56c381eb2d64e730489ea713aa5f40d4
EricDuong <ericlamduong@gmail.com>
2024-02-29 18:04:51 -0600

    testing ultrasonic library

--- 2023-2024-MST-R5.ino
+++ 2023-2024-MST-R5.ino
@@ -1,12 +1,14 @@
 
 #include "Robot.h"
 #include "IEEE_Pinout.h"
+#include <HCSR04.h>
+
 
 Motor fr(FRONT_RIGHT_PWM, FRONT_RIGHT_DIR, FRONT_MOTORS_ENABLE, false);
 Motor fl(FRONT_LEFT_PWM, FRONT_LEFT_DIR, FRONT_MOTORS_ENABLE, false);
 Motor br(BACK_RIGHT_PWM, BACK_RIGHT_DIR, BACK_MOTORS_ENABLE, false);
 Motor bl(BACK_LEFT_PWM, BACK_LEFT_DIR, BACK_MOTORS_ENABLE, true);
-
+HCSR04 hc(HC_TRIGGER, HC_ECHO)
 
 
 
@@ -26,5 +28,14 @@ void loop() {
   robot.turn(CW, 70, 1500);
   robot.turn(CCW, 70, 1500);
  
+  //square();
+  Serial.println(hc.dist() );
+  delay(60);
 }
 
+void square() {
+  robot.drive(FORWARD, 100, 1000);
+  robot.drive(RIGHT, 100, 1000);
+  robot.drive(BACKWARD, 100, 1000);
+  robot.drive(LEFT, 100, 1000);
+}

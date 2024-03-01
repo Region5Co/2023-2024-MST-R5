2b1c40ba8e55f1e9a9877143b16b84a3053f63b3
KatieM-mst <kgmghk@mst.edu>
2024-02-29 18:25:51 -0600

    Update 2023-2024-MST-R5.ino

--- 2023-2024-MST-R5.ino
+++ 2023-2024-MST-R5.ino
@@ -20,13 +20,36 @@ void setup() {
 }
 
 void loop() {
-  robot.drive(FORWARD, 100, 1500);
+  /*robot.drive(FORWARD, 100, 1500);
   robot.drive(BACKWARD, 100, 1500);
   robot.drive(LEFT, 100, 1500);
   robot.drive(RIGHT, 100, 1500);
   robot.turn(CW, 100, 1500);
-  robot.turn(CCW, 100, 1500);
- 
+  robot.turn(CCW, 100, 1500);*/
+  switch(stage){
+    case 0:
+    {
+      if(robot.right_distance >= inches)
+      {
+        robot.drive_right(forward);
+      }else{
+        robot.stop();
+        stage = 1;
+      }
+      break;
+    }
+    case 1:
+    {
+      if(robot.forward_distance() >= y_inches)
+      {
+        robot.move(forward);
+      }else
+      {
+        robot.stop();
+        stage = 2;
+      }
+    }
+  }
 }
 
 

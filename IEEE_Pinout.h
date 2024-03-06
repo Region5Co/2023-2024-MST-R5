6b64a8abf12b01abdd3828e21184404052485119
Isaac Coppage <icoppage@mst.edu>
2024-03-05 19:43:43 -0600

    Fixed Pinout

--- IEEE_Pinout.h
+++ IEEE_Pinout.h
@@ -15,17 +15,7 @@
   #define SD1 MOSI
   #define SD2 9
   #define SD3 10
-#else
-  #define D0 0
-  #define D1 1
-  #define D2 2
-  #define D3 3
-  #define D4 4
-  #define D5 5
-  #define D6 6
-  #define D7 7
-  #define D8 8
-#endif
+
 
 #define FRONT_MOTORS_ENABLE   D0
 #define BACK_MOTORS_ENABLE    D0

1843f392184f587f6bd45a6e520dc5378a4cdd0a
TREX1345 <128239723+TREX1345@users.noreply.github.com>
2024-04-03 21:27:13 -0500

    Update IEEE_Pinout.h
    had way outdated pinout

--- IEEE_Pinout.h
+++ IEEE_Pinout.h
@@ -1,55 +1,61 @@
 #ifndef IEEE_PINOUT_H
+#define IEEE_DEBUG false
 
-
-
-
-#define D0 16
-#define D1 5
-#define D2 4
-#define D3 0
-#define D4 2
-#define D5 14
-#define D6 12
-#define D7 13
-#define D8 15
-#define SD1 MOSI
-#define SD2 9
-#define SD3 10
-
-//#define D0 0
-//#define D1 1
-//#define D2 2
-//#define D3 3
-//#define D4 4
-//#define D5 5
-//#define D6 6
-//#define D7 7
-//#define D8 8
-//#define D9 9
-//#define D10 10
-//#define D11 11
-//#define D12 12
-//#define D13 13
-
-#define A0 A0 
-#define A1 A1
-#define A2 A2
-#define A3 A3
-#define A4 A4
-#define A5 A5
+#define D0   0
+#define D1   1
+#define D2   2
+#define D3   3
+#define D4   4
+#define D5   5
+#define D6   6
+#define D7   7
+#define D8   8
+#define D9   9
+#define D10  10
+#define D11  11
+#define D12  12  
+#define D13  13 
+#define D14  14 
+#define D15  15 
+#define D16  16 
+#define D17  17 
+#define D18  18 
+#define D19  19 
+#define D20  20 
+#define D21  21 
+#define D22  22 
+#define D23  23 
+#define D20  20 
+#define D21  21 
+#define D22  22 
+#define D23  23 
+#define D24  24 
+#define D25  25 
+#define D26  26 
+#define D27  27
+#define D28  28 
+#define D29  29 
+#define D30  30 
+#define D31  31  
+ 
+#if IEEE_DEBUG
+    #define IEEE_SERIAL 1
+#endif
 
 #define FRONT_MOTORS_ENABLE   D0
 #define BACK_MOTORS_ENABLE    D0
-#define FRONT_RIGHT_PWM       D3
-#define FRONT_RIGHT_DIR       D4
-#define FRONT_LEFT_PWM        D7
-#define FRONT_LEFT_DIR        D8
-#define BACK_RIGHT_PWM        D1
-#define BACK_RIGHT_DIR        D2
-#define BACK_LEFT_PWM         D5
-#define BACK_LEFT_DIR         D6
-#define HC_TRIGGER            1
-#define HC_ECHO               3
-#define SERVO_PIN             D0
+#define FRONT_RIGHT_PWM       D4
+#define FRONT_RIGHT_DIR       D28
+#define FRONT_LEFT_PWM        D6
+#define FRONT_LEFT_DIR        D24
+#define BACK_RIGHT_PWM        D5
+#define BACK_RIGHT_DIR        D26
+#define BACK_LEFT_PWM         D7
+#define BACK_LEFT_DIR         D22
+
+#define IEEE_B0               A1 //Test for uno
+#define IEEE_B1               A0 //Test for uno
+
+#define SERVO_PIN             50
 
 #endif

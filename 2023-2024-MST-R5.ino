9d861d7c796ffe32eac7820980fec80f77113f43
TREX1345 <128239723+TREX1345@users.noreply.github.com>
2024-04-04 20:52:55 -0500

    Update 2023-2024-MST-R5.ino
    adding gyro stuff

--- 2023-2024-MST-R5.ino
+++ 2023-2024-MST-R5.ino
@@ -6,6 +6,19 @@
 #include "Adafruit_VL53L1X.h"
 #include <vl53l1x_class.h>
 #include <vl53l1x_error_codes.h>
+#include "src/Adafruit_LSM6DS/Adafruit_LSM6DS3TRC.h"
+
+#define MAX_T_INDEX 7
+
+#if !defined(ACCEL_RANGE) && !defined(ACCEL_RATE) && IEEE_ACCEL
+#define ACCEL_RANGE LSM6DS_ACCEL_RANGE_16_G
+#define ACCEL_RATE  LSM6DS_RATE_1_66K_HZ
+#endif
+
+#if !defined(GYRO_RANGE) && !defined(GYRO_RATE)
+#define GYRO_RANGE  LSM6DS_GYRO_RANGE_500_DPS
+#define GYRO_RATE   LSM6DS_RATE_1_66K_HZ
+#endif
 
 Motor fr(FRONT_RIGHT_PWM, FRONT_RIGHT_DIR, FRONT_MOTORS_ENABLE, false);
 Motor fl(FRONT_LEFT_PWM, FRONT_LEFT_DIR, FRONT_MOTORS_ENABLE, false);
@@ -13,6 +26,11 @@ Motor br(BACK_RIGHT_PWM, BACK_RIGHT_DIR, BACK_MOTORS_ENABLE, false);
 Motor bl(BACK_LEFT_PWM, BACK_LEFT_DIR, BACK_MOTORS_ENABLE, true);
 Servo myservo;
 Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(21, 20);
+Adafruit_LSM6DS3TRC imu;
+
+//IMU
+Gyro gyro(false,true);
+
 
 Encoder encFL(E_FRONT_LEFT_INT, E_FORNT_LEFT_DIR);
 Encoder encFR(E_FRONT_RIGHT_INT, E_FRONT_RIGHT_DIR);
@@ -48,6 +66,20 @@ void setup() {
   vl53.setTimingBudget(500);
   vl53.VL53L1X_SetDistanceMode(2);
   vl53.VL53L1X_SetROI(4, 16);
+
+    //Imu initialization
+  if(!imu.begin_I2C(0x6B)){
+    Serial.println("ERROR Initializing I2C for IMU");
+  }
+  /*Not using the accelerometer*/
+  //imu.setAccelRange(ACCEL_RANGE);
+  //imu.setAccelDataRate(ACCEL_RATE);
+  imu.setGyroRange(GYRO_RANGE);
+  imu.setGyroDataRate(GYRO_RATE);
+  //have to check this portion
+  //not using Accel.
+  //imu.configInt1(false, false, true); // accelerometer DRDY on INT1 of the imu
+  imu.configInt1(false, true, false); // gyro DRDY on INT1 of the imu
 
   attachInterrupt(digitalPinToInterrupt(encFL.getEncIntPin()), interruptEncoderFL, RISING);
   attachInterrupt(digitalPinToInterrupt(encFR.getEncIntPin()), interruptEncoderFR, RISING);
